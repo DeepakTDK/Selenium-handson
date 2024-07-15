e997c36f09d3b4f3579b709c377400946ef5ae06
DeepakTDK <deepaktmdofficial@gmail.com>
2024-07-15 17:39:24 +0530

    implicit wait

---
+++ src/main/java/org/deepak/day5/ImplicitwaitDemo.java
@@ -0,0 +1,20 @@
+package org.deepak.day5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ImplicitwaitDemo {
+    public static void main(String[] args){
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+    }
+}
